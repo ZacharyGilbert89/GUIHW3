6ba383a46be61c8266e8862f668b1ba31fc9aecf
Zach_Gilbert <zach.gilbert2020@gmail.com>
2023-10-26 11:51:28 -0400

    Update

---
+++ index.js
@@ -0,0 +1,16 @@
+var minCol = 0;
+var minRow = 0;
+var maxCol = 0;
+var maxRow = 0;
+
+function submitRange () {
+    //alert("Button Pressed");
+    minCol = document.getElementById("minCol").value;
+    maxCol = document.getElementById("maxCol").value;
+    minRow = document.getElementById("minRow").value;
+    maxRow = document.getElementById("maxRow").value;
+    //alert(minCol + "\n" + maxCol + "\n" + minRow + "\n" + maxRow + "\n");
+}
+function createTable() {
+    
+}
